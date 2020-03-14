57a2e01cd5c9639248617f7c97b248f4c66970ae
emirnur <nur.emirlan@gmail.com>
2020-03-14 10:20:39 +0600

    добавлена модель файла

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    sign = models.CharField(max_length=200, verbose_name='Подпись')
+    file = models.FileField(upload_to='uploads', verbose_name='Файл')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE,
+                             verbose_name='Пользователь', related_name='file_author')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.sign
+
